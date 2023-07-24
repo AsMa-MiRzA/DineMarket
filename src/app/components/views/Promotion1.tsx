5a48b280d3bb4ca483fd4d0f946740f2efe3faf0
AsMa-MiRzA <asma_mansha@yahoo.com>
2023-07-24 17:54:58 +0500

    Update Promotion1.tsx

--- src/app/components/views/Promotion1.tsx
+++ src/app/components/views/Promotion1.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@<prefix>/components/ui/button'
 import React from 'react'
-
+import Wrapper from '@<prefix>/components/wrapper'
 const Promotion1 = () => {
   return (
+  <Wrapper>
     <div className=' border-r-stone-800 bg-neutral-800 mt-5 text-center w-3/6 h-48 bg-fixed '>
     <h1 className='text-4xl text-white font-extrabold text-center py-10'>GET 30% OFF</h1>
     <p className='text-white font-semi-bold text-center -mt-6'>USE PROMO CODE</p>
     <p className='mb-2.5 text-white text-lg bg-neutral-700 px-28 py-2 mx-28 text-justify rounded'>DINEWEEKENDSALE</p>
 </div>
+    </Wrapper>
   )
 }
 export default Promotion1
